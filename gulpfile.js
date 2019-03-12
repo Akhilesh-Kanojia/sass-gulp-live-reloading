569c97963a94f5dbfe63e8995748e89c5a3c51e2
Akhilesh Kanojia <kanojiaakhilesh2010@gmail.com>
2019-03-13 00:04:16 +0530

    added js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create(); // For live reloading
+var reload = browserSync.reload;
 
 gulp.task('sass', function() {
  return gulp.src('./sass/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 
+});
+
+gulp.task('concat', function(){
+   return gulp.src('./js/*.js')
+          .pipe(concat('common.js')) // common.js will going to create in js folder
+          .pipe(gulp.dest('js'));
 });
 
 //gulp.task('watch', function() {
@@ -16,15 +24,29 @@ gulp.task('sass', function() {
 
 /*gulp.task('default', ['sass', 'watch']);*/
   
-gulp.task('serve', ['sass'], function() {
+ 
+ gulp.task('serve', ['sass', 'concat'], function() {
 	
-	browserSync.init({
-		server: './'
-	});
+ 	browserSync.init({
+ 		server: './'
+ 	});
 
-	gulp.watch('./sass/*.scss', ['sass']);
-	gulp.watch('./*.html').on('change', browserSync.reload);
+ 	gulp.watch('./sass/*.scss', ['sass']);
+ 	gulp.watch('./*.html').on('change', browserSync.reload);
 
-});
+ });
 
 gulp.task('default', ['serve']);
+
+/*gulp.task('browserSync', function(){
+  browserSync.init({
+		server: './'
+	});
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.watch('./sass/*.scss', ['sass']);
+gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browserSync', 'sass', 'watch']);*/
